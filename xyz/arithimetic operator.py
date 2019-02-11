2673a0041f07559c64e026ddb5b70b284c877302
NeerajGodiyal <47513872+NeerajGodiyal@users.noreply.github.com>
2019-02-11 11:53:11 +0530

    Create arithimetic operator.py

---
+++ xyz/arithimetic operator.py
@@ -0,0 +1,13 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+ch=int(input("Enter an arithimetic op:"))
+if ch=='+':
+     print("Result:",(a+b))
+elif ch=="-":
+     print("Result:",(a-b))
+elif ch=="*":
+     print("Result:",(a*b))
+elif ch=="/"
+     print("Result:",(a/b))
+else:
+print("Result:",(a%b))
