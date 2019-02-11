b365b08025b960eb7f64f4b172b6021e7ba34517
NeerajGodiyal <47513872+NeerajGodiyal@users.noreply.github.com>
2019-02-11 12:12:00 +0530

    Create print th e largest of three.py

---
+++ xyz/print th e largest of three.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first no:"))
+b-int(input("Enter the second no:"))
+c-int(input("Enter the third no:"))
+max=0
+if a>b and a>c:
+     max=a
+if b>a and b>c:
+     max=b
+if c>a and c>b:
+     max=c
+print("LArgest number:",max)	 
